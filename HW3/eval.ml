6924aadf62eb07e23e79d98634e9d73463064810
ChrisXu1006 <xuruilxt@gmail.com>
2014-09-23 14:21:14 -0400

    Adding evaluation of boolean operation

--- HW3/eval.ml
+++ HW3/eval.ml
@@ -27,12 +27,28 @@ let make_configuration (c:com) : configuration =
 (* evaluate a aexp *)
 let rec evala (a:aexp) : int = 
     match a with 
-    | Int m -> m
-	| Var x -> Hashtbl.find sigma x
-    | Plus(a1, a2) -> (evala a1) + (evala a2)
-  	| Minus(a1, a2) -> (evala a1) - (evala a2)
-  	| Times(a1, a2) -> (evala a1) * (evala a2)
-  	| Input -> sp ">"; 1   
+    | Int                 m -> m
+	| Var                 x -> Hashtbl.find sigma x
+    | Plus         (a1, a2) -> (evala a1) + (evala a2)
+  	| Minus        (a1, a2) -> (evala a1) - (evala a2)
+  	| Times        (a1, a2) -> (evala a1) * (evala a2)
+  	| Input                 -> sp ">"; 1   
+
+(* evaluate a bexp *)
+let rec evalb (boo:bexp) : bool = 
+    match boo with
+    | True                  -> true
+    | False                 -> false
+    | Equals  (aexp1, aexp2)-> if ((evala aexp1) == (evala aexp2)) then true else false
+    | NotEquals(aexp1,aexp2)-> if ((evala aexp1) <> (evala aexp2)) then true else false
+    | Less    (aexp1, aexp2)-> if ((evala aexp1) <  (evala aexp2)) then true else false
+    | LessEq  (aexp1, aexp2)-> if ((evala aexp1) <= (evala aexp2)) then true else false
+    | Greater (aexp1, aexp2)-> if ((evala aexp1) >  (evala aexp2)) then true else false
+    | GreaterEq(aexp1,aexp2)-> if ((evala aexp1) >= (evala aexp2)) then true else false
+    | Not              boo  -> if (evalb boo) then false else true
+    | And     (bexp1, bexp2)-> (evalb bexp1) && (evalb bexp2)
+    | Or      (bexp1, bexp2)-> (evalb bexp1) || (evalb bexp2) 
+
 
 (* evaluate a command *)
 let rec evalc (conf:configuration) : store = 
