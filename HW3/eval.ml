6f62c0690aaeb9fafbdf84681f2315acfab47441
ChrisXu1006 <xuruilxt@gmail.com>
2014-09-23 13:12:56 -0400

    Modified eval

--- HW3/eval.ml
+++ HW3/eval.ml
@@ -1,4 +1,7 @@
 open Ast
+open Hashtbl
+open Printf
+open Pprint
 
 (* Interpreter exceptions *)
 exception IllegalBreak
@@ -7,15 +10,31 @@ exception TestFailure of string
 exception UnboundVariable of var
     
 (* A type for stores *)
-type store = (* FILL ME IN *) unit
-
+type store =
+    ( var, int ) Hashtbl.t;;
+    
 (* A type for configurations *)
-type configuration = (* FILL ME IN *) unit
+type configuration = store * com;;
+
+(* Initialize the store for the program*)
+let ( sigma : store ) = Hashtbl.create 200;;
 
 (* create an initial configuration from a command *)
 let make_configuration (c:com) : configuration = 
-  failwith "Not yet implemented"
+    ( sigma , c );;
+    (* failwith "Not yet implemented" *)
+
+(* evaluate a aexp *)
+let rec evala (arithmetic:aexp) : int = 
+    match arithmetic with 
+    | Int arithmetic -> arithmetic
 
 (* evaluate a command *)
 let rec evalc (conf:configuration) : store = 
-  failwith "Not yet implemented"
+    match (snd conf) with 
+    | Assign (left, right) ->  Hashtbl.add sigma left (evala right);
+      print_int (Hashtbl.find sigma left);
+      sigma  
+     (*| _ failwith "Not yet implemented"*)
+
+
