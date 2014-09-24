280bea53e649406e1c7d1dd010d07eb02a5d0747
Yao Wang <yw438@cornell.edu>
2014-09-23 22:10:02 -0400

    add aexp and bexp configuration

--- HW3/eval.ml
+++ HW3/eval.ml
@@ -15,60 +15,62 @@ type store =
     
 (* A type for configurations *)
 type configuration = store * com;;
+type aconfiguration = store * aexp;;
+type bconfiguration = store * bexp;;
 
 (* Initialize the store for the program*)
 let ( sigma : store ) = Hashtbl.create 200;;
 
 (* create an initial configuration from a command *)
 let make_configuration (c:com) : configuration = 
-    ( sigma , c );;
+	( sigma , c );;
     (* failwith "Not yet implemented" *)
 
 (* evaluate an aexp *)
-let rec evala (ari:aexp) : int = 
-    match ari with 
-    | Int               ari -> ari
-    | Var               ari -> Hashtbl.find sigma ari
-    | Plus    (aexp1, aexp2)-> (evala aexp1) + (evala aexp2)
-    | Minus   (aexp1, aexp2)-> (evala aexp1) - (evala aexp2)
-    | Times   (aexp1, aexp2)-> (evala aexp1) * (evala aexp2)
-    | Input                 -> print_endline ">";
-                               let i = read_int() in i
+let rec evala (aconf:aconfiguration) : int = 
+    match aconf with 
+    | (sigma, Int               ari )-> ari
+    | (sigma, Var               ari )-> Hashtbl.find sigma ari
+    | (sigma, Plus    (aexp1, aexp2))-> (evala (sigma, aexp1)) + (evala (sigma, aexp2))
+    | (sigma, Minus   (aexp1, aexp2))-> (evala (sigma, aexp1)) - (evala (sigma, aexp2))
+    | (sigma, Times   (aexp1, aexp2))-> (evala (sigma, aexp1)) * (evala (sigma, aexp2))
+    | (sigma, Input                 )-> print_endline ">";
+                               			let i = read_int() in i
 
 (* evaluate a bexp *)
-let rec evalb (boo:bexp) : bool = 
-    match boo with
-    | True                  -> true
-    | False                 -> false
-    | Equals  (aexp1, aexp2)-> if ((evala aexp1) == (evala aexp2)) then true else false
-    | NotEquals(aexp1,aexp2)-> if ((evala aexp1) <> (evala aexp2)) then true else false
-    | Less    (aexp1, aexp2)-> if ((evala aexp1) <  (evala aexp2)) then true else false
-    | LessEq  (aexp1, aexp2)-> if ((evala aexp1) <= (evala aexp2)) then true else false
-    | Greater (aexp1, aexp2)-> if ((evala aexp1) >  (evala aexp2)) then true else false
-    | GreaterEq(aexp1,aexp2)-> if ((evala aexp1) >= (evala aexp2)) then true else false
-    | Not              boo  -> if (evalb boo) then false else true
-    | And     (bexp1, bexp2)-> (evalb bexp1) && (evalb bexp2)
-    | Or      (bexp1, bexp2)-> (evalb bexp1) || (evalb bexp2) 
+let rec evalb (bconf:bconfiguration) : bool = 
+    match bconf with
+    | (sigma, True)                  -> true
+    | (sigma, False)                 -> false
+    | (sigma, Equals  (aexp1, aexp2))-> if ((evala (sigma, aexp1)) == (evala (sigma, aexp2))) then true else false
+    | (sigma, NotEquals(aexp1,aexp2))-> if ((evala (sigma, aexp1)) <> (evala (sigma, aexp2))) then true else false
+    | (sigma, Less    (aexp1, aexp2))-> if ((evala (sigma, aexp1)) <  (evala (sigma, aexp2))) then true else false
+    | (sigma, LessEq  (aexp1, aexp2))-> if ((evala (sigma, aexp1)) <= (evala (sigma, aexp2))) then true else false
+    | (sigma, Greater (aexp1, aexp2))-> if ((evala (sigma, aexp1)) >  (evala (sigma, aexp2))) then true else false
+    | (sigma, GreaterEq(aexp1,aexp2))-> if ((evala (sigma, aexp1)) >= (evala (sigma, aexp2))) then true else false
+    | (sigma, Not              bexp )-> if (evalb (sigma, bexp)) then false else true
+    | (sigma, And     (bexp1, bexp2))-> (evalb (sigma, bexp1)) && (evalb (sigma, bexp2))
+    | (sigma, Or      (bexp1, bexp2))-> (evalb (sigma, bexp1)) || (evalb (sigma, bexp2)) 
 
 (* evaluate a command *)
-let rec evalc (conf:configuration) : store = 
-    match conf with 
+let rec evalc (cconf:configuration) : store = 
+    match cconf with 
     | (sigma, Skip)             ->  sigma
     | (sigma, Assign (x, a))    ->  if (Hashtbl.mem sigma x) 
-									then Hashtbl.replace sigma x (evala a)
-									else Hashtbl.add sigma x (evala a); 
+									then Hashtbl.replace sigma x (evala (sigma, a))
+									else Hashtbl.add sigma x (evala (sigma, a)); 
 									sigma
     | (sigma, Seq  (c1, c2))    ->  (evalc (sigma, c1));
                                     (evalc (sigma, c2))
-    | (sigma, If(b, c1, c2))    ->  if (evalb b)
+    | (sigma, If(b, c1, c2))    ->  if (evalb (sigma, b))
                                     then (evalc (sigma, c1))
                                     else (evalc (sigma, c2))
-    | (sigma, While  (b, c))    ->  if (evalb b)
+    | (sigma, While  (b, c))    ->  if (evalb (sigma, b))
                                     then (evalc (sigma, c);
                                     evalc (sigma, While(b, c)))
                                     else (evalc (sigma, Skip))
-    | (sigma, Print a      )    ->  Printf.printf "%d\n" (evala a); sigma
-    | (sigma, Test  (i,  b))    ->  if (evalb b)
+    | (sigma, Print a      )    ->  Printf.printf "%d\n" (evala (sigma, a)); sigma
+    | (sigma, Test  (i,  b))    ->  if (evalb (sigma, b))
                                     then (evalc (sigma, Skip))
                                     else (Printf.printf "TestFailed\n";
                                     pprintInfo i;
